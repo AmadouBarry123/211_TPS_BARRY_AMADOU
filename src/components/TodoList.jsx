24af22321d3930659cfe1bb15010782f1b735f5a
AmadouBarry123 <2241112@collegemv.qc.ca>
2025-04-15 11:17:24 -0400

    Ajout de la logique de creation de tache . Prochainnement il faut cree Taches.jsx et a laide de props generer les taches

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,34 @@
 import '../css/TodoList.css'
+import { useState } from 'react';
 
 function TodoList(){
+    const [tache , setTache] = useState("");
+    const [taches , setTaches] = useState([]);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setTaches([...taches, tache]);
+        setTache("")
+    }
+
     return (
             <main>
                 <label className='All' htmlFor="">
                     <h1 className="MainTitle">Todo List</h1>
                     <div className='inputGroup'>
-                        <input className='inputTache' type="text" placeholder='Ajouter une nouvelle tache' />
-                        <button className='BonttonAjouter'>Ajouter</button>
+                        <form action="" onSubmit={handleSubmit}>
+                            <input  className='inputTache' type="text" placeholder='Ajouter une nouvelle tache' 
+                            value={tache}
+                            onChange={(e) => setTache(e.target.value)}
+                            />
+                            <button className='BonttonAjouter'>Ajouter</button>
+                        </form>
+                        <ul className='listeTaches'>
+                            {taches.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+
                     </div>
                 </label>
             </main>
