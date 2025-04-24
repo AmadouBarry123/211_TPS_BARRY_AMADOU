0ac77cd990a41d29acac5ae1a77949f51e8fdb5b
AmadouBarry123 <2241112@collegemv.qc.ca>
2025-04-24 16:14:27 -0400

    Ajout de quelques elements faut ajouter le fetch()

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+
+export default function App() {
+  const [name, setName] = React.useState("")
 
 
-
-function App() {
-
-
-
-
-  return(
-    <>
-
-      salut
-    </>
+  return (
+      <>
+        <Header/>
+        <MainContent/>
+      </>
   )
-
 }
-export default App
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../css/Header.css'
+
+function Header(){
+
+    return(
+    <header>
+        <h1>MEME GENERATOR <span> <h5> Tp3</h5> </span></h1>
+    </header>
+    )
+
+}
+
+export default Header
