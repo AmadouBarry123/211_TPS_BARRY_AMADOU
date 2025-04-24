1a73b1546cf73b80c599601351cc86f70eb75f3d
Amadou Hambaliou Barry <2241112@collegemv.qc.ca>
2025-04-24 14:56:31 -0400

    Clean du code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
-import MainSection from './components/MainSection';
-import Footer from './components/Footer';
-import ProfileSection from './components/ProfileSection'
-import Card from './components/Card';
-import cards from './assets/cards';
+
+
 
 function App() {
 
@@ -13,13 +9,8 @@ function App() {
 
   return(
     <>
-      <Header/>
-      <MainSection/>
-      {/* <div>{cardElements}</div> */}
 
-      <Footer/>
-
-      
+      salut
     </>
   )
 
