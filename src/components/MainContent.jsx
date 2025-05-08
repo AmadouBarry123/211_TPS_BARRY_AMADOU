59f793b35277c77d13b5df23b1a35252fdaef9a2
AmadouBarry123 <2241112@collegemv.qc.ca>
2025-05-08 18:42:01 -0400

    Ajout de un peu de style et de la deuxieme partie de la page principale

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,39 +4,47 @@ import chillGuy from '../assets/chillguy2.webp';
 
 function MainContent() {
   return (
-    <div className="presentationPerso">
-      <h2>Présentation personnelle</h2>
-      
-      <img src={chillGuy} alt="" />
-      <p>
-        Je me nomme Amadou Barry et je suis un étudiant 
-        en informatique au collège Marie-Victorin.
-      </p>
+    <div>
+      <div className="presentationPerso">
+        <h2>Présentation personnelle</h2>
+        
+        <img src={chillGuy} alt="" />
+        <p>
+          Je me nomme Amadou Barry et je suis un étudiant 
+          en informatique au collège Marie-Victorin.
+        </p>
 
-      <div className="traitsEtInterets">
-        <div className="colonne">
-          <h3 className="traitDeCaratere">Traits de caractère</h3>
-          <ul>
-            <li>Généreux</li>
-            <li>Emphatique</li>
-            <li>Méfiant</li>
-            <li>Créatif</li>
-            <li>Persévérant</li>
-            <li>Curieux</li>
-          </ul>
-        </div>
+        <div className="traitsEtInterets">
+          <div className="colonne">
+            <h3 className="traitDeCaratere">Traits de caractère</h3>
+            <ul>
+              <li>Généreux</li>
+              <li>Emphatique</li>
+              <li>Méfiant</li>
+              <li>Créatif</li>
+              <li>Persévérant</li>
+              <li>Curieux</li>
+            </ul>
+          </div>
 
-        <div className="colonne">
-          <h3 className="interet">Centres d'intérêt</h3>
-          <ul>
-            <li>Informatique</li>
-            <li>Jeux vidéo</li>
-            <li>Manga</li>
-            <li>Sport</li>
-            <li>Dormir</li>
-            <li>Musique</li>
-          </ul>
+          <div className="colonne">
+            <h3 className="interet">Centres d'intérêt</h3>
+            <ul>
+              <li>Informatique</li>
+              <li>Jeux vidéo</li>
+              <li>Manga</li>
+              <li>Sport</li>
+              <li>Dormir</li>
+              <li>Musique</li>
+            </ul>
+          </div>
         </div>
+      </div>
+
+     {/* Presentation Professionnelle */}
+
+      <div className="presentationProfessionnelle">
+        <h2>Présentation Professionnelle</h2>
       </div>
     </div>
   );
