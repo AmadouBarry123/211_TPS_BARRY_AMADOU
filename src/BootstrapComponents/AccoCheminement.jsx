32edaa8806f893a1b06280837b7bcffe35cc0853
AmadouBarry123 <2241112.collegemv.qc.ca>
2025-05-25 22:45:06 -0400

    Ajout de mes infos et mes projets

--- src/BootstrapComponents/AccoCheminement.jsx
+++ src/BootstrapComponents/AccoCheminement.jsx
@@ -3,30 +3,44 @@ import "../css/MainContent.css";
 
 // https://react-bootstrap.netlify.app/docs/components/accordion/
 function AccoCheminement() {
-  return (
+return (
     <Accordion defaultActiveKey="0">
-      <Accordion.Item  eventKey="0">
-        <Accordion.Header> Cheminement scolaire et formations</Accordion.Header>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Cheminement scolaire et formations</Accordion.Header>
         <Accordion.Body>
-        <ul>
-          <li> Primaire à l'ecole Simone-Desjardins / Date :  </li>
-        </ul>
+          <ul>
+            <li>Primaire à l'école Simone-Desjardins / Date : 2017 (fin)</li>
+            <li>Secondaire à l'école secondaire Henri-Bourassa / Date : 2017 (début) – 2022 (fin)</li>
+            <li>Cégep Marie-Victorin, Techniques de l'informatique / Date : 2022 (début) – En cours...</li>
+            <li>
+              Formation Médaille de bronze / Croix de bronze / Premiers soins généraux / Sauveteur national (Société de sauvetage du Québec) <br />
+              Dates : 2015 (début) – 2024 (fin)
+            </li>
+          </ul>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
         <Accordion.Header>Emplois et stages</Accordion.Header>
         <Accordion.Body>
-
+          <ul>
+            <li>Associé au restaurant McDonald's / Date : 2017 (début) – 2020 (fin)</li>
+            <li>Aide à domicile pour personne à mobilité réduite / Date : 2015 – En cours...</li>
+            <li>Préparateur de commandes / Date : tout l'été 2024</li>
+            <li>Associé Home Depot (saisonnier) / En cours...</li>
+          </ul>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2">
         <Accordion.Header>Activités parascolaires ou compétitions</Accordion.Header>
         <Accordion.Body>
-
+          <ul>
+            <li>Karaté de 2011 à 2020</li>
+            <li>Compétition de karaté à 3 reprises</li>
+          </ul>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
-  );
+);
 }
 
 export default AccoCheminement;
--- src/assets/projet.js
+++ src/assets/projet.js
@@ -1,33 +1,33 @@
 import chillGuy from './chillguy2.webp';
+import animation from './deepProjetAnimation.png';
+import game from './GameProject.png';
+import art from './ProjetArtInfo.png';
 
 const projets = [
     {
         id: 1,
-        titre: "Meme Generator",
-        description: "Un site web pour créer et gérer des memes",
-        technologies: "HTML, CSS  , JavaScript et React",
-        dateRealisation: "2023-01-15",
-        image: chillGuy,
+        titre: "Animator Roblox",
+        description: "Dans ce projet, j'ai créé un animateur pour Roblox qui permet de créer des animations personnalisées pour les personnages du jeu.",
+        technologies: "Lua, Roblox Studio",
+        dateRealisation: "2023",
+        image: animation,
     },
-        {
+    {
         id: 2,
-        titre: "Meme Generator",
-        description: "Un site web pour créer et gérer des memes",
-        technologies: "HTML, CSS  , JavaScript et React",
-        dateRealisation: "2023-01-15",
-        image: chillGuy,
+        titre: "Jeu de plateforme",
+        description: "Un jeu de plateforme en 2D où le joueur doit traverser des niveaux en sautant sur des plateformes et choisir la bonne réponse pour passer au niveau suivant.",
+        technologies: "React, JavaScript, HTML, CSS, Chromecast, Firebase, PixiJS",
+        dateRealisation: "2025",
+        image: game,
     },
-            {
-        id: 2,
-        titre: "Meme Generator",
-        description: "Un site web pour créer et gérer des memes",
-        technologies: "HTML, CSS  , JavaScript et React",
+    {
+        id: 3,
+        titre: "Maison hantée",
+        description: "À l'intérieur de cette maison, il y a un fantôme qui hante les lieux. Grâce à la communication dans un topic MQTT d'un Raspberry Pi, j'allume les fantômes à l'intérieur.",
+        technologies: "Python, Raspberry Pi, MQTT",
         dateRealisation: "2023-01-18",
-        image: chillGuy,
+        image: art,
     },
-    
-    
-
 ];
 
 export default projets;
